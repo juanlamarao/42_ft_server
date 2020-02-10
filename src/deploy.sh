09c182525b6653e09d223e1b7ef7dbd62ad57df6
Juan Lamarão <oliveiralamarao@gmail.com>
2020-02-10 16:25:47 -0300

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -50,7 +50,7 @@ chmod 600 /etc/ssl/certs/localhost.dev.crt /etc/ssl/private/localhost.dev.key
 
 #install phpmyadmin
 #wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
+tar -xvf phpmyadmin.tar.gz
 mv phpMyAdmin-4.9.0.1-all-languages /var/www/localhost/phpmyadmin
 cp -pr ./config.ini.php /var/www/localhost/phpmyadmin/config.inc.php
 cp -pr ./nginx.conf /etc/nginx/sites-available/default
