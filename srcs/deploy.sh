88a5956a503bf0c57002ac20f0b18ba797594d41
Juan Lamarão <oliveiralamarao@gmail.com>
2020-02-11 14:04:53 -0300

    Update deploy.sh

--- srcs/deploy.sh
+++ srcs/deploy.sh
@@ -12,25 +12,25 @@
 # **************************************************************************** #
 
 #configure timezone
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime 2>> /root/erros.txt
 
 #update system
 #apt-get update
 
 #install mysql & nginx & php
 #echo -e "\nInstalling Nginx Mysql and PHP packages.."
-aptitude install -y mariadb-server nginx php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline php-json php-mbstring php7.3-mbstring php-curl php-gd php-intl php-soap php-xml php-xmlrpc php-zip #> /dev/null
+aptitude install -y mariadb-server nginx php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline php-json php-mbstring php7.3-mbstring php-curl php-gd php-intl php-soap php-xml php-xmlrpc php-zip 2>> /root/erros.txt
 #echo -e ".......................................\e[32m\e[1mOK\e[0m"
 
 #create web directory and change to root dir
-mkdir /var/www/localhost
-cd /root
+mkdir /var/www/localhost 2>> /root/erros.txt
+cd /root 2>> /root/erros.txt
 
 #start mysql & nginx
 #echo -e "\nStarting services.."
-/etc/init.d/mysql start
-/etc/init.d/nginx start
-/etc/init.d/php7.3-fpm start
+/etc/init.d/mysql start 2>> /root/erros.txt
+/etc/init.d/nginx start 2>> /root/erros.txt
+/etc/init.d/php7.3-fpm start 2>> /root/erros.txt
 #echo -e "....................\e[32m\e[1mOK\e[0m"
 
 #configure SSL
@@ -38,56 +38,56 @@ cd /root
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
     -subj "/C=BR/ST=Sao Paulo/L=Sao Paulo/O=localhost/OU=Development/CN=localhost/emailAddress=juolivei@localhost.dev" \
     -keyout localhost.dev.key \
-    -out localhost.dev.crt # > /dev/null
-mv localhost.dev.crt /etc/ssl/certs/
-mv localhost.dev.key /etc/ssl/private/
-chmod 600 /etc/ssl/certs/localhost.dev.crt /etc/ssl/private/localhost.dev.key
+    -out localhost.dev.crt  2>> /root/erros.txt
+mv localhost.dev.crt /etc/ssl/certs/ 2>> /root/erros.txt
+mv localhost.dev.key /etc/ssl/private/ 2>> /root/erros.txt
+chmod 600 /etc/ssl/certs/localhost.dev.crt /etc/ssl/private/localhost.dev.key 2>> /root/erros.txt
 #echo -e ".......................\e[32m\e[1mOK\e[0m"
 
 #install phpmyadmin
 #echo -e "Installing PhpMyAdmin.."
 #wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar -xvf phpmyadmin.tar.gz
-mv phpMyAdmin-4.9.0.1-all-languages /var/www/localhost/phpmyadmin
-cp -pr ./config.inc.php /var/www/localhost/phpmyadmin/config.inc.php
-cp -pr ./php.ini /etc/php/7.3/fpm/php.ini
-cp -pr ./nginx.conf /etc/nginx/sites-available/default
+tar -xvf phpmyadmin.tar.gz 2>> /root/erros.txt
+mv phpMyAdmin-4.9.0.1-all-languages /var/www/localhost/phpmyadmin 2>> /root/erros.txt
+cp -pr ./config.inc.php /var/www/localhost/phpmyadmin/config.inc.php 2>> /root/erros.txt
+cp -pr ./php.ini /etc/php/7.3/fpm/php.ini 2>> /root/erros.txt
+cp -pr ./nginx.conf /etc/nginx/sites-available/default 2>> /root/erros.txt
 #chmod 755 /var/www/localhost
-chown -R www-data:www-data /var/www/localhost/phpmyadmin
-echo "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'juolivei'@'localhost' IDENTIFIED BY 'juolivei';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
+chown -R www-data:www-data /var/www/localhost/phpmyadmin 2>> /root/erros.txt
+echo "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'juolivei'@'localhost' IDENTIFIED BY 'juolivei';" | mysql -u root 2>> /root/erros.txt
+echo "FLUSH PRIVILEGES;" | mysql -u root 2>> /root/erros.txt
 #echo -e ".....................\e[32m\e[1mOK\e[0m"
 
 #install wordpress
 #echo "\nInstalling wordpress.."
 #curl -LO https://wordpress.org/latest.tar.gz
-tar -xvf latest.tar.gz
-cp wp-config.php wordpress/wp-config.php
-mv wordpress /var/www/localhost/
-chown -R www-data:www-data /var/www/localhost/wordpress
-echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
-echo "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'juolivei';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
+tar -xvf latest.tar.gz 2>> /root/erros.txt
+cp wp-config.php wordpress/wp-config.php 2>> /root/erros.txt
+mv wordpress /var/www/localhost/ 2>> /root/erros.txt
+chown -R www-data:www-data /var/www/localhost/wordpress 2>> /root/erros.txt
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root 2>> /root/erros.txt
+echo "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'juolivei';" | mysql -u root 2>> /root/erros.txt
+echo "FLUSH PRIVILEGES;" | mysql -u root 2>> /root/erros.txt
 #echo -e "....................\e[32m\e[1mOK\e[0m"
 
 #import mysql wordpress dump
 #echo "Importing wordpress database schemas.."
-mysql -u root wordpress < wordpress.sql
+mysql -u root wordpress < wordpress.sql 2>> /root/erros.txt
 #echo -e "...................................\e[32m\e[1mOK\e[0m"
 
 #clean cache
 #echo "\nCleaning cache.."
-apt-get clean -y # > /dev/null
-apt-get autoclean -y # > /dev/null
-apt-get autoremove -y # > /dev/null
-rm -f /root/php.ini
-rm -f /root/deploy.sh
-rm -f /root/nginx.conf
-rm -f /root/latest.tar.gz
-rm -f /root/wp-config.php
-rm -f /root/config.inc.php
-rm -f /root/phpmyadmin.tar.gz
-rm -f /root/wordpress.sql
+apt-get clean -y 2>> /root/erros.txt
+apt-get autoclean -y 2>> /root/erros.txt
+apt-get autoremove -y 2>> /root/erros.txt
+rm -f /root/php.ini 2>> /root/erros.txt
+rm -f /root/deploy.sh 2>> /root/erros.txt
+rm -f /root/nginx.conf 2>> /root/erros.txt
+rm -f /root/latest.tar.gz 2>> /root/erros.txt
+rm -f /root/wp-config.php 2>> /root/erros.txt
+rm -f /root/config.inc.php 2>> /root/erros.txt
+rm -f /root/phpmyadmin.tar.gz 2>> /root/erros.txt
+rm -f /root/wordpress.sql 2>> /root/erros.txt
 #echo -e "..............\e[32m\e[1mOK\e[0m"
 
 #echo -e "\n _______"
